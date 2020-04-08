e35f0b6eba6d840ac33f17b6b4a6530238e8f8e6
MVCionOld <tsionmichel1999@gmail.com>
2020-04-08 05:29:56 +0300

    Added RSA encoding

--- app/src/main/java/com/learning/helloworld/LoginActivity.kt
+++ app/src/main/java/com/learning/helloworld/LoginActivity.kt
@@ -2,12 +2,16 @@ package com.learning.helloworld
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.content.edit
+import androidx.appcompat.app.AppCompatActivity
+import java.security.Key
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import javax.crypto.Cipher
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,22 +31,48 @@ class LoginActivity : AppCompatActivity() {
         realLogin = preferences.getString(KEY_LOGIN, "")!!
         realPassword = preferences.getString(KEY_PASSWORD, "")!!
 
+        val keyPairGen: KeyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        keyPairGen.initialize(1024)
+
+        val keyPair: KeyPair = keyPairGen.genKeyPair()
+        val publicKey: Key = keyPair.public
+        val privateKey: Key = keyPair.private
+
+        val cipher: Cipher = Cipher.getInstance("RSA")
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey)
+
+        if (realPassword.isEmpty()) { // kostyli i govnokod
+            realPassword = Base64.encodeToString(
+                cipher.doFinal(realPassword.toByteArray()),
+                Base64.DEFAULT
+            )
+        }
+
         registerButton.setOnClickListener {
             val login = loginEdit.text.toString()
             val password = passwordEdit.text.toString()
+            val encodedPassword: String = Base64.encodeToString(
+                cipher.doFinal(password.toByteArray()),
+                Base64.DEFAULT
+            )
+
             val preferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
             val editor = preferences.edit()
             editor.putString(KEY_LOGIN, login)
-            editor.putString(KEY_PASSWORD, password)
+            editor.putString(KEY_PASSWORD, encodedPassword)
             editor.apply()
         }
 
         confirmButton.setOnClickListener {
             val login = loginEdit.text.toString()
             val password = passwordEdit.text.toString()
+            val encodedPassword: String = Base64.encodeToString(
+                cipher.doFinal(password.toByteArray()),
+                Base64.DEFAULT
+            )
 
             if (login == realLogin) {
-                if (password == realPassword) {
+                if (encodedPassword == realPassword) {
                     val intent = Intent(this, MainActivity::class.java)
                     intent.putExtra(KEY_LOGIN, login)
                     startActivity(intent)
